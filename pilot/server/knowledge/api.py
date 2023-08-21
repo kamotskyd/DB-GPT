e7d11584895edc705c705b577413eac574636d27
Denis Kamotsky <129412308+kamotskyd@users.noreply.github.com>
2023-08-21 05:36:35 -0600

    Update api.py

--- pilot/server/knowledge/api.py
+++ pilot/server/knowledge/api.py
@@ -30,7 +30,7 @@ router = APIRouter()
 
 
 #embeddings = HuggingFaceEmbeddings(model_name=LLM_MODEL_CONFIG[CFG.EMBEDDING_MODEL])
-embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+embeddings = HuggingFaceEmbeddings(model_name=CFG.EMBEDDING_MODEL)
 
 knowledge_space_service = KnowledgeService()
 
